c36ee7ac3b3fd2e6860087f15f3194775ae68f8b
Niko Kristian Pinnis <niko.pinnis@gmail.com>
2019-01-24 20:34:18 +0200

    Section 6 done

---
+++ server/src/helpers/renderer.js
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { renderToString } from 'react-dom/server';
+import Home from '../client/components/Home';
+import { StaticRouter } from 'react-router-dom';
+import Routes from '../client/Routes';
+import { Provider } from 'react-redux';
+
+export default (req, store) => {
+  const content = renderToString(
+    <Provider store={store}>
+      <StaticRouter location={req.path} context={{}}>
+        <Routes />
+      </StaticRouter>
+    </Provider>
+  );
+
+  return `
+    <html>
+        <head></head>
+        <body>
+            <div id="root">${content}</div>
+            <script src="bundle.js"></script>
+        </body>
+    </html>
+  `;
+};
--- server/webpack.client.js
+++ server/webpack.client.js
@@ -1,6 +1,8 @@
 const path = require('path');
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.base');
 
-module.exports = {
+const config = {
   // tell webpack the root file
   entry: './src/client/client.js',
 
@@ -10,23 +12,7 @@ module.exports = {
     path: path.resolve(__dirname, 'public')
     // library: 'server',
     // libraryTarget: 'commonjs2'
-  },
-
-  // run babel on every file it runs through
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        options: {
-          presets: [
-            'react',
-            'stage-0',
-            ['env', { targets: { browsers: ['last 2 versions'] } }]
-          ]
-        }
-      }
-    ]
   }
 };
+
+module.exports = merge(baseConfig, config);
